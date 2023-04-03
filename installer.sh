d3a4f99b577975d7b271389568ca65bfbb98f589
tkeil <tkeil@users.noreply.github.com>
2023-04-03 14:56:29 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -111,9 +111,9 @@ DESCRIPTION
     This is the install wrapper that allows to run an installer with one command in a remote manner.
 
     Sample:
-    curl -L https://raw.githubusercontent.com/GITHUB_ORG_NAME/GITHUB_REPO_NAME/master/${0} | sh
+    curl -fsSL https://raw.githubusercontent.com/GITHUB_ORG_NAME/GITHUB_REPO_NAME/master/${0} | sh
     or
-    curl -L https://raw.githubusercontent.com/GITHUB_ORG_NAME/GITHUB_REPO_NAME/master/${0} | sh -s -- --github-repo-name GITHUB_ORG_REPO_NAME --installer-filename FILENAME
+    curl -fsSL https://raw.githubusercontent.com/GITHUB_ORG_NAME/GITHUB_REPO_NAME/master/${0} | sh -s -- --github-repo-name GITHUB_ORG_REPO_NAME --installer-filename FILENAME
     or
     wget -q -O - https://raw.githubusercontent.com/GITHUB_ORG_NAME/GITHUB_REPO_NAME/${0} | sh
 
@@ -206,7 +206,7 @@ tmpDir="$(mktemp -d -t ${__INSTALLER_PROJECT_NAME}-installer.XXXXXXXXXXXXXXXXXXX
     || stop_it "Can't create temporary directory for downloading ASV-SAM-Installer")"
 
 # Git latest release of installer file
-INSTALLER_URL=$(curl -sL ${__INSTALLER_GITHUB_RELEASE_URI} \
+INSTALLER_URL=$(curl -fsSL ${__INSTALLER_GITHUB_RELEASE_URI} \
     | grep "browser_download_url" \
     | cut -d : -f 2,3 | tr -d \" | sed 's/ //g' | grep "${__INSTALLER_INSTALLER_FILENAME}$")
 echo "INSTALLER_URL=${INSTALLER_URL}"
@@ -218,7 +218,7 @@ fi
 
 # Get installer - Download in temp directory
 echo "Download '${INSTALLER_URL}' to temporary file '${__INSTALLER_INSTALLER_FILENAME}' ..."
-curl --fail -sL ${INSTALLER_URL} -o "$tmpDir/${__INSTALLER_INSTALLER_FILENAME}" \
+curl -fsSL ${INSTALLER_URL} -o "$tmpDir/${__INSTALLER_INSTALLER_FILENAME}" \
     || stop_it "Could not load latest installer version from '${INSTALLER_URL}'. Please refer to: ${__INSTALLER_GITHUB_URI}"
 
 # Get installer gpg signature - Download in temp directory
@@ -226,7 +226,7 @@ INSTALLER_SIG_URL=${INSTALLER_URL}.sig
 ${__INSTALLER_SKIP_SIGNATURE_VALIDATION} || \
     echo "Download '${INSTALLER_SIG_URL}' to temporary file '${__INSTALLER_INSTALLER_SIG_FILENAME}' ..."
 ${__INSTALLER_SKIP_SIGNATURE_VALIDATION} || \
-    curl --fail -sL ${INSTALLER_SIG_URL} -o "$tmpDir/${__INSTALLER_INSTALLER_SIG_FILENAME}" \
+    curl -fsSL ${INSTALLER_SIG_URL} -o "$tmpDir/${__INSTALLER_INSTALLER_SIG_FILENAME}" \
         || stop_it "Could not load installer signature from '${INSTALLER_SIG_URL}'. Please refer to: ${__INSTALLER_GITHUB_URI}"
 
 # Validate gpg signature of installer
